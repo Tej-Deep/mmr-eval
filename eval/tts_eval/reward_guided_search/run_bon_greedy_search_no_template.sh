711ad3d533b6ee0b1e13dab5c514a9b2d3f7c410
ob1 <ob1@nscc.com>
2025-10-03 14:04:02 +0800

    new file

---
+++ eval/tts_eval/reward_guided_search/run_bon_greedy_search_no_template.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Add the parent directory to PYTHONPATH so imports work correctly
+# export PYTHONPATH="${PYTHONPATH}:/home/ubuntu/porialab-us-midwest-1/Tej/mmr-eval/mmr-eval"
+export PYTHONPATH="${PYTHONPATH}:/scratch_aisg/SPEC-SF-AISG/ob1/mmr-eval"
+
+source /scratch_aisg/SPEC-SF-AISG/ob1/mmr-eval/qwen-evaluation/.venv/bin/activate
+echo "Python path after activation: $(which python)"
+echo "Python version: $(python --version)"
+
+cd reward_guided_search/
+
+# dataset_list=("mmmu_dev" "mathvista_testmini")
+dataset_list=(
+    "mathvista_testmini"
+    # "MMMU_DEV_VAL" 
+    # "puzzleVQA_1K_subset" 
+    # "AlgoPuzzleVQA_900_subset"
+    # "mathvision_test"
+)
+
+# POLICY_MODEL_PATH="Qwen/Qwen2.5-VL-7B-Instruct"
+# POLICY_MODEL_PATH="google/gemma-3-12b-it"
+POLICY_MODEL_PATH="openbmb/MiniCPM-V-2_6" # 8B
+# POLICY_MODEL_PATH="OpenGVLab/InternVL2_5-8B"
+
+CHECKPOINT_BASE_PATH="/scratch_aisg/SPEC-SF-AISG/ob1/prm-training-code/training_outputs"
+checkpoint="${CHECKPOINT_BASE_PATH}/Q3B_mc0_sr_mc0_balanced_bs2_gs4_lr1e-5_VF_0827_1452"
+
+# Performing Search and Generating Responses
+for dataset in "${dataset_list[@]}"; do
+    echo "Dataset: $dataset"
+    CUDA_VISIBLE_DEVICES=0,1 python vllm_bon_greedy_search_no_template.py \
+        --policy_model_path $POLICY_MODEL_PATH \
+        --reward_model_path $checkpoint \
+        --data $dataset \
+        --output_dir ./outputs/DEBUG/$dataset-results \
+        --development_mode
+done
+
+
+# Evaluating Responses
+
+# python collate_final_eval_results.py path_to_json_result_file.json
